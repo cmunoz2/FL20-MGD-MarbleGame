171d3f6194cf3c2767d2a5acb4c1200453eacbca
cmunoz2 <munozc12399@gmail.com>
2020-10-12 23:55:48 -0500

    Levels/Code Added

---
+++ Assets/Scripts/ChangeLevels.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLevels : MonoBehaviour
+{
+    [Tooltip("Name of the scene you want to go to.")]
+    public string destination;
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+            if(destination == "") destination = "MainMenu";
+           UnityEngine.SceneManagement.SceneManager.LoadScene(destination);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public bool phoneIsConnected = false;
+
+    [HideInInspector]
+    public Vector3 dir, startPosition;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.R)) ResetPlayer();
+        if(this.transform.position.y < -2) ResetPlayer();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        dir = Vector3.zero;
+        if(phoneIsConnected){
+            dir.x = Input.acceleration.x;
+            dir.z = Input.acceleration.y;
+        } else {
+            dir.x = Input.GetAxis("Horizontal");
+            dir.z = Input.GetAxis("Vertical");
+        }
+
+        rb.AddForce(dir * 5);
+    }
+
+    public void ResetPlayer(){
+        rb.velocity = Vector3.zero;
+        this.transform.position = startPosition;
     }
 }
